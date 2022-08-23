d09926581cfa8c13b701f39d731d31edba20605c
fabogit <fabo.dev.email@gmail.com>
2022-08-23 21:26:19 +0200

    css Highlighting a Calendar Date
    TODO fix highlight-date class

--- angular-material-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ angular-material-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,33 +1,46 @@
-import {Component} from '@angular/core';
-import {UntypedFormBuilder, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { UntypedFormBuilder, Validators } from '@angular/forms';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
 
 @Component({
-  selector: "create-course-step-1",
-  templateUrl:"create-course-step-1.component.html",
-  styleUrls: ["create-course-step-1.component.scss"]
+	selector: "create-course-step-1",
+	templateUrl: "create-course-step-1.component.html",
+	styleUrls: ["create-course-step-1.component.scss"]
 })
 export class CreateCourseStep1Component {
 
-  form = this.fb.group({
-    title: ['', [
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(60)
-    ]],
-    releasedAt: [new Date(2000, 0, 1), Validators.required],
-    category: ['BEGINNER', Validators.required],
-    courseType: ['premium', Validators.required],
-    downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
-  });
+	form = this.fb.group({
+		title: ['', [
+			Validators.required,
+			Validators.minLength(5),
+			Validators.maxLength(60)
+		]],
+		releasedAt: [new Date(2000, 0, 1), Validators.required],
+		category: ['BEGINNER', Validators.required],
+		courseType: ['premium', Validators.required],
+		downloadsAllowed: [false, Validators.requiredTrue],
+		longDescription: ['', [Validators.required, Validators.minLength(3)]]
+	});
 
-  constructor(private fb: UntypedFormBuilder) {
+	dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
 
-  }
+		const date = cellDate.getDate();
 
-  get courseTitle() {
-    return this.form.controls['title'];
-  }
+		if (view == 'month') {
+
+			return (date == 1) ? 'highlight-date' : "";
+		}
+
+		return "";
+	};
+
+	constructor(private fb: UntypedFormBuilder) {
+
+	}
+
+	get courseTitle() {
+		return this.form.controls['title'];
+	}
 
 }
