b6bf93165920819aedddcf656651a27cf9353b33
fabogit <fabo.dev.email@gmail.com>
2022-08-25 16:31:33 +0200

    Data Table - Loading Data From the Backend
    rxjs pipe tap subscribe

--- angular-material-course/src/app/services/courses.service.ts
+++ angular-material-course/src/app/services/courses.service.ts
@@ -1,56 +1,53 @@
-
-
-import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Course} from "../model/course";
-import {map} from "rxjs/operators";
-import {Lesson} from "../model/lesson";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Course } from "../model/course";
+import { map } from "rxjs/operators";
+import { Lesson } from "../model/lesson";
 
 
 @Injectable()
 export class CoursesService {
 
-    constructor(private http:HttpClient) {
+	constructor(private http: HttpClient) {
+	}
 
-    }
+	findCourseById(courseId: number): Observable<Course> {
+		return this.http.get<Course>(`http://localhost:9001/api/courses/${courseId}`);
+	}
 
-    findCourseById(courseId: number): Observable<Course> {
-        return this.http.get<Course>(`http://localhost:9001/api/courses/${courseId}`);
-    }
+	findAllCourses(): Observable<Course[]> {
+		return this.http.get('http://localhost:9001/api/courses')
+			.pipe(
+				map(res => res['payload'])
+			);
+	}
 
-    findAllCourses(): Observable<Course[]> {
-        return this.http.get('http://localhost:9001/api/courses')
-            .pipe(
-                map(res => res['payload'])
-            );
-    }
+	findAllCourseLessons(courseId: number): Observable<Lesson[]> {
+		return this.http.get('http://localhost:9001/api/lessons', {
+			params: new HttpParams()
+				.set('courseId', courseId.toString())
+				.set('pageNumber', "0")
+				.set('pageSize', "1000")
+		}).pipe(
+			map(res => res["payload"])
+		);
+	}
 
-    findAllCourseLessons(courseId:number): Observable<Lesson[]> {
-        return this.http.get('http://localhost:9001/lessons', {
-            params: new HttpParams()
-                .set('courseId', courseId.toString())
-                .set('pageNumber', "0")
-                .set('pageSize', "1000")
-        }).pipe(
-            map(res =>  res["payload"])
-        );
-    }
+	findLessons(
+		courseId: number, sortOrder = 'asc',
+		pageNumber = 0, pageSize = 3, sortColumn = 'seqNo'): Observable<Lesson[]> {
 
-    findLessons(
-        courseId:number, sortOrder = 'asc',
-        pageNumber = 0, pageSize = 3, sortColumn = 'seqNo'):  Observable<Lesson[]> {
-
-        return this.http.get('http://localhost:9001/lessons', {
-            params: new HttpParams()
-                .set('courseId', courseId.toString())
-                .set('sortOrder', sortOrder)
-                .set('pageNumber', pageNumber.toString())
-                .set('pageSize', pageSize.toString())
-                .set('sortColumn', sortColumn)
-        }).pipe(
-            map(res =>  res["payload"])
-        );
-    }
+		return this.http.get('http://localhost:9001/api/lessons', {
+			params: new HttpParams()
+				.set('courseId', courseId.toString())
+				.set('sortOrder', sortOrder)
+				.set('pageNumber', pageNumber.toString())
+				.set('pageSize', pageSize.toString())
+				.set('sortColumn', sortColumn)
+		}).pipe(
+			map(res => res["payload"])
+		);
+	}
 
 }
